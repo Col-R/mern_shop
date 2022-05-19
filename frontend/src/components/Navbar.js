e82f109c9be2aeeee5deb53f0832ffda7f571c33
Col-R <2colewestin@gmail.com>
2022-05-19 15:34:20 -0500

    navbar styling

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -11,12 +11,14 @@ const Navbar = () => {
         {/*links */}
         <ul className = "navbar__links">
             <li>
-                <Link to ="/cart">
+                <Link to ="/cart" className="cart__link">
                     {/* Icon */}
                     {/* Importing fontawesome icon from cdnjs.com into public/index.html*/}
                     <i className = "fas fa-shopping-cart"></i>
+                    <span>
                     Cart
                     <span className = "cartlogo__badge">0</span>
+                    </span>
                 </Link>
             </li>
             <li>
