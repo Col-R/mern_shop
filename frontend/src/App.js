c68a3e978cacd75aa7b044a0c192522b2f575348
Col-R <2colewestin@gmail.com>
2022-05-19 11:21:38 -0500

    frontend up and running, skeleton navbar complete. Updated react-router-dom dependency reference on app.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,27 @@
 import './App.css';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 // Screens
 import HomeScreen from './screens/HomeScreen';
 import ProductScreen from './screens/ProductScreen';
 import CartScreen from './screens/CartScreen';
 
+// components
+import Navbar from './components/Navbar'
+
 function App() {
   return (
     <Router>
     {/*Navbar for desktop*/}
+    <Navbar />
     {/* SideDrawer for mobile*/}
     {/* Backdrop */}
     <main>
-      <Switch>
+      <Routes> {/* Apparently this used to be Switch but that changed in react-router-dom v6, https://stackoverflow.com/questions/63124161/attempted-import-error-switch-is-not-exported-from-react-router-dom */}
         <Route exact path = "/" component = {HomeScreen} />
         <Route exact path = "/product/:id" component = {ProductScreen}/>
         <Route exact path = "/cart" component = {CartScreen}/>
-      </Switch>
+      </Routes>
     </main>
     
     </Router>
--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -1,5 +1,5 @@
 import './Navbar.css';
-
+import {Link} from 'react-router-dom';
 
 const Navbar = () => {
     return (
@@ -12,11 +12,25 @@ const Navbar = () => {
         <ul className = "navbar__links">
             <li>
                 <Link to ="/cart">
-
+                    {/* Icon */}
+                    {/* Importing fontawesome icon from cdnjs.com into public/index.html*/}
+                    <i className = "fas fa-shopping-cart"></i>
+                    Cart
+                    <span className = "cartlogo__badge">0</span>
+                </Link>
+            </li>
+            <li>
+                <Link to ="/shop">
+                    Shop
                 </Link>
             </li>
         </ul>
         {/*hamburger menu*/}
+        <div className = "hamburger__menu">
+            <div></div>
+            <div></div>
+            <div></div>
+        </div>
         </nav>
     )
 }
