fd150741de11f34193dedddbef8dde252a539d1a
Col-R <2colewestin@gmail.com>
2022-05-23 08:39:16 -0500

    side drawer hooks completed

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,5 @@
 import './App.css';
+import {useState} from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 // Screens
@@ -9,15 +10,19 @@ import CartScreen from './screens/CartScreen';
 // components
 import Navbar from './components/Navbar'
 import Backdrop from './components/Backdrop'
+import SideDrawer from './components/SideDrawer'
 
 function App() {
+
+  const [sideToggle, setSideToggle] = useState(false);
   return (
     <Router>
     {/*Navbar for desktop*/}
     <Navbar />
     {/* SideDrawer for mobile*/}
+    <SideDrawer show = {sideToggle}/>
     {/* Backdrop */}
-    <Backdrop />
+    <Backdrop show = {sideToggle}/>
     <main>
       <Routes> {/* Apparently this used to be Switch but that changed in react-router-dom v6, https://stackoverflow.com/questions/63124161/attempted-import-error-switch-is-not-exported-from-react-router-dom */}
         <Route exact path = "/" component = {HomeScreen} />
