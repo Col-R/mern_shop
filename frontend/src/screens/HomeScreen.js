c8b4aa02b0ba506220f7e159f915abcd8e7dc831
Col-R <2colewestin@gmail.com>
2022-05-25 11:00:32 -0500

    add actions to homeScreen and proxy to outer json file

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -1,18 +1,27 @@
 import './HomeScreen.css'
+import {useEffect} from 'react';
+import {useSelector, useDispatch} from 'react-redux'; // react hooks
 
 // components
 import Product from '../components/Product';
 
+// actions
+import {getProducts as listProducts} from '../redux/actions/productActions';
+
 const HomeScreen = () => {
+
+  const dispatch = useDispatch();
+  const getProducts = useSelector(state => state.getProducts);
+  const { products, loading, error } = getProducts;
+
+  useEffect (() => {
+    dispatch(listProducts())
+  }, [dispatch]);
+
   return <div className = "homescreen">
     <h2 className = "homescreen__title">Latest Products</h2>
     <div className = "homescreen__products">
-      <Product />
-      <Product />
-      <Product />
-      <Product />
-      <Product />
-      <Product />
+
     </div>
   </div>
 }
