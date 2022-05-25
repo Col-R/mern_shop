335e44a388b63684974dde5025888f364d1c48ef
Col-R <2colewestin@gmail.com>
2022-05-25 10:37:39 -0500

    cart, product, product detail reducers complete

--- frontend/src/redux/reducers/cartReducers.js
+++ frontend/src/redux/reducers/cartReducers.js
@@ -3,7 +3,7 @@ import * as actionTypes from '../constants/cartConstants';
 export const cartReducer = (state = {cartItems:[]}, action) => {
     switch(action.type) {
         case actionTypes.ADD_TO_CART:
-            const item = actionm.payload;
+            const item = action.payload;
             const existItem = state.cartItems.find((x) => x.product === item.product);
 
             if (existItem) {
--- frontend/src/redux/store.js
+++ frontend/src/redux/store.js
@@ -3,10 +3,13 @@ import thunk from 'redux-thunk';
 import {composeWithDevTools} from 'redux-devtools-extension';
 
 // Reducers
-import { cartReducer } from './reducers/cartReducers'
+import { cartReducer } from './reducers/cartReducers';
+import { getProductsReducer, getProductDetailsReducer } from './reducers/productReducers';
 
 const reducer = combineReducers({
     cart: cartReducer,
+    getProducts: getProductsReducer,
+    getProductDetails: getProductDetailsReducer,
 });
 
 const middleware = [thunk];
