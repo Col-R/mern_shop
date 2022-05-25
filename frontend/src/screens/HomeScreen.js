260f6e2352e6cf5b4c55ba4bf6d7790e2a5ed41d
Col-R <2colewestin@gmail.com>
2022-05-25 11:17:54 -0500

    finished homescreen product display

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -21,7 +21,20 @@ const HomeScreen = () => {
   return <div className = "homescreen">
     <h2 className = "homescreen__title">Latest Products</h2>
     <div className = "homescreen__products">
-
+    {loading ? (
+      <h2>Loading...</h2>
+    ) : error ? (
+      <h2>{error}</h2>
+    ) : ( 
+      products.map((product) => <Product 
+        key = {product._id}
+        productId = {product._id}
+        name = {product.name}
+        price = {product.price}
+        description = {product.description}
+        imageUrl = {product.imageUrl}
+      />)
+    )}
     </div>
   </div>
 }
